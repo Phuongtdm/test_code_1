d1ced3e08cfc3695d51d28c2c4748106a01aed8c
PhuongVH <vhphuong89@gmail.com>
2024-10-25 14:59:17 +0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,6 @@
+python3 -m pip install --upgrade pip
+# pip install catboost
+# pip install --upgrade tensorflow
+# pip install --upgrade numpy pandas
+pip install scikit-learn
+python ./test_code_1/code.py
