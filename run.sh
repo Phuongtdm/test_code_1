97b1f70a575c8be4d1d179f7125bbc753164dc14
Phuongtdm <vhphuong89@gmail.com>
2024-10-26 17:44:40 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 python3 -m pip install --upgrade pip
 # pip install catboost
 # pip install --upgrade tensorflow
-# pip install --upgrade numpy pandas
+pip install --upgrade numpy
+# pip install --upgrade pandas
 pip install scikit-learn
 python ./test_code_1/code.py
